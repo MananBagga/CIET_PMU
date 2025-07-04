81d3e6e1cb4b282b2fd08cccf9d7c28ac3a6f959
Manan Bagga <mananbagga0310@gmail.com>
2025-07-04 14:45:47 +0530

    add home page template and db

--- ciet_pmu/user_admin/models.py
+++ ciet_pmu/user_admin/models.py
@@ -22,3 +22,75 @@ class programs(models.Model):
     program_types = models.CharField(max_length=6, choices=PROGRAM_TYPES, default='OTHERS')
     # assign_coordinator = models.ForeignKey(coordinator, on_delete=models.CASCADE, related_name='assign_coordinator')
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Annualbudget(models.Model):
+#     year = models.IntegerField()
+#     budget = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+#     created_at = models.DateTimeField(blank=True, null=True)
+#     updated_at = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'AnnualBudget'
+
+
+# class Budgetsummary(models.Model):
+#     year = models.IntegerField()
+#     month = models.IntegerField()
+#     total_expenditure = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+#     created_at = models.DateTimeField(blank=True, null=True)
+#     updated_at = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'BudgetSummary'
+
+
+# class Program(models.Model):
+#     type = models.TextField(blank=True, null=True)
+#     title = models.TextField()
+#     coordinator = models.ForeignKey('User', models.DO_NOTHING)
+#     annual_budget = models.ForeignKey(Annualbudget, models.DO_NOTHING)
+#     created_at = models.DateTimeField(blank=True, null=True)
+#     updated_at = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'Program'
+
+
+# class Subentry(models.Model):
+#     task = models.ForeignKey('Task', models.DO_NOTHING)
+#     title = models.TextField()
+#     description = models.TextField(blank=True, null=True)
+#     date = models.DateField()
+#     budget = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+#     created_at = models.DateTimeField(blank=True, null=True)
+#     updated_at = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'SubEntry'
+
+
+# class Task(models.Model):
+#     program = models.ForeignKey(Program, models.DO_NOTHING)
+#     task_type = models.TextField(blank=True, null=True)
+#     created_at = models.DateTimeField(blank=True, null=True)
+#     updated_at = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'Task'
