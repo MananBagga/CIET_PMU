09957245b25b04544b8e2c7f953e7745295e8abb
Manan Bagga <mananbagga0310@gmail.com>
2025-07-08 11:53:07 +0530

    add admin login changes

--- ciet_pmu/admin_login/views.py
+++ ciet_pmu/admin_login/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def admin_login(request):
-    """
-    Render the admin login page.
-    """
+
+    if request.method == 'GET':
+        username = request.GET.get('username', '').strip()
+        password = request.GET.get('password', '')
+
+    if not username or not password:
+            messages.error(request, 'All fields are required.')
+            return render(request, 'login/login.html')
+    
+    if Admin.objects.filter(username=username, password=password).exists():
+        return redirect('user_dashboard')
+    else:
+        messages.error(request, 'Invalid username or password.')
+        # user = User.objects.get(username=username, password=password)
+        # if user.role == 'Coordinator':
+        # else:
+        #     messages.error(request, 'You do not have permission to access this page.')
+        #     return render(request, 'login/login.html')
+
     return render(request, 'admin_login/admin_login.html')
