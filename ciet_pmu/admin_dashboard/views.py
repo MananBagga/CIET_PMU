b79919ad8adfb9ea9eca9cc5a7b3df00e1881060
Manan Bagga <mananbagga0310@gmail.com>
2025-07-10 11:58:24 +0530

    add chart.js and bar chart ui

--- ciet_pmu/admin_dashboard/views.py
+++ ciet_pmu/admin_dashboard/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def admin_dashboard(request):
-    """
-    Render the admin dashboard page.
-    """
-    return render(request, 'admin_dashboard/admin_dashboard.html')
+    labels = ['Q1', 'Q2', 'Q3', 'Q4']
+    data = [25, 40, 30, 60]
+
+    context = {
+        'labels': json.dumps(labels),
+        'data': json.dumps(data)
+    }
+    return render(request, 'admin_dashboard/admin_dashboard.html', context)
 
 def budget(request):
-    """
-    Render the set annual budget page.
-    """
     return render(request, 'admin_dashboard/budget.html')
 
 def projects(request):
-    """
-    Render the projects page.
-    """
     return render(request, 'admin_dashboard/projects.html')
