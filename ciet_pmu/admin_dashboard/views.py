181f56edd09bc4dacaed4f67436baded762886ac
Manan Bagga <mananbagga0310@gmail.com>
2025-07-11 10:02:22 +0530

    fix program form

--- ciet_pmu/admin_dashboard/views.py
+++ ciet_pmu/admin_dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import json
+from .models import User
 
 # Create your views here.
 def admin_dashboard(request):
@@ -16,4 +17,5 @@ def budget(request):
     return render(request, 'admin_dashboard/budget.html')
 
 def projects(request):
-    return render(request, 'admin_dashboard/projects.html')
+    users = User.objects.all()
+    return render(request, 'admin_dashboard/projects.html', {'users': users})
