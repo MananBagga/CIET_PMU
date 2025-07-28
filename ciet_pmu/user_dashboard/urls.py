88198b0ba10899aa27752ddbdd3c0ae8dc90d4e6
Manan Bagga <mananbagga0310@gmail.com>
2025-07-28 10:50:37 +0530

    fix user dashboard form

--- ciet_pmu/user_dashboard/urls.py
+++ ciet_pmu/user_dashboard/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path('<int:user_id>/user_dashboard/', views.user_dashboard, name='user_dashboard'),
     path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
     path('<int:program_id>/edit_program/', views.edit_program, name='edit_program'),
-    # path('save_program_details/', views.save_program_details, name='save_program_details'),
+    path('delete-subentry/<int:entry_id>/', views.delete_subentry, name='delete_subentry'),
 ]
--- ciet_pmu/user_dashboard/views.py
+++ ciet_pmu/user_dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from admin_dashboard.models import Program, User, Subentry
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def user_dashboard(request):
     user_id = request.session.get('user_id')
@@ -15,99 +17,81 @@ def user_dashboard(request):
         'subtask':subtask
     })
 
+def save_entries(ids, names, dates, budgets, entry_type, program_id):
+    program = Program.objects.get(pk=program_id) 
+    for entry_id, name, date, budget in zip(ids, names, dates, budgets):
+        if entry_id: 
+            try:
+                sub = Subentry.objects.get(id=entry_id)
+                sub.subtask_name = name
+                sub.subtask_date = date
+                sub.subtask_budget = budget
+                sub.save()
+            except Subentry.DoesNotExist:
+                pass
+        else:  
+            Subentry.objects.create(
+                program=program,      
+                subtask=entry_type,
+                subtask_name=name,
+                subtask_date=date,
+                subtask_budget=budget
+            )
+
+
 def edit_program(request, program_id):
     program = get_object_or_404(Program, pk=program_id)
 
     if request.method == 'POST':
+        kpi_ids = request.POST.getlist('kpi_id[]')
         kpi_names = request.POST.getlist('kpi_name[]')
         kpi_dates = request.POST.getlist('kpi_date[]')
         kpi_budgets = request.POST.getlist('kpi_budget[]')
 
+        workshop_ids = request.POST.getlist('workshop_id[]')
         workshop_names = request.POST.getlist('workshop_name[]')
         workshop_dates = request.POST.getlist('workshop_date[]')
         workshop_budgets = request.POST.getlist('workshop_budget[]')
 
-        meetings_names = request.POST.getlist('meeting_name[]')
-        meetings_dates = request.POST.getlist('meeting_date[]')
-        meetings_budgets = request.POST.getlist('meeting_budget[]')
+        meeting_ids = request.POST.getlist('meeting_id[]')
+        meeting_names = request.POST.getlist('meeting_name[]')
+        meeting_dates = request.POST.getlist('meeting_date[]')
+        meeting_budgets = request.POST.getlist('meeting_budget[]')
 
+        manpower_ids = request.POST.getlist('manpower_id[]')
         manpower_names = request.POST.getlist('manpower_details[]')
         manpower_dates = request.POST.getlist('manpower_date[]')
         manpower_budgets = request.POST.getlist('manpower_budget[]')
 
-        for name, date, budget in zip(kpi_names, kpi_dates, kpi_budgets):
-            Subentry.objects.create(
-                program_id=program_id,
-                program=program,
-                subtask="kpi",
-                subtask_name=name,
-                subtask_date=date,
-                subtask_budget=budget
-            )
-        for name, date, budget in zip(workshop_names, workshop_dates, workshop_budgets):
-            Subentry.objects.create(
-                program_id=program_id,
-                program=program,
-                subtask="workshop",
-                subtask_name=name,
-                subtask_date=date,
-                subtask_budget=budget
-            )
-        for name, date, budget in zip(meetings_names, meetings_dates, meetings_budgets):
-            Subentry.objects.create(
-                program_id=program_id,
-                program=program,
-                subtask="meetings",
-                subtask_name=name,
-                subtask_date=date,
-                subtask_budget=budget
-            )
-        for name, date, budget in zip(manpower_names, manpower_dates, manpower_budgets):
-            Subentry.objects.create(
-                program_id=program_id,
-                program=program,
-                subtask="manpower",
-                subtask_name=name,
-                subtask_date=date,
-                subtask_budget=budget
-            )
+        save_entries(kpi_ids, kpi_names, kpi_dates, kpi_budgets, "kpi", program_id)
+        save_entries(workshop_ids, workshop_names, workshop_dates, workshop_budgets, "workshop", program_id)
+        save_entries(meeting_ids, meeting_names, meeting_dates, meeting_budgets, "meetings", program_id)
+        save_entries(manpower_ids, manpower_names, manpower_dates, manpower_budgets, "manpower", program_id)
 
-        # return redirect('program_detail', program_id=program.id)
+        return redirect('user_dashboard')
 
 
-    return render(request, 'user_dashboard/edit_program.html', {'program': program})
+    kpi_entries = Subentry.objects.filter(program=program, subtask="kpi")
+    workshop_entries = Subentry.objects.filter(program=program, subtask="workshop")
+    meeting_entries = Subentry.objects.filter(program=program, subtask="meetings")
+    manpower_entries = Subentry.objects.filter(program=program, subtask="manpower")
+
+    return render(request, 'user_dashboard/edit_program.html', {
+        'program': program,
+        'kpi_entries': kpi_entries,
+        'workshop_entries': workshop_entries,
+        'meeting_entries': meeting_entries,
+        'manpower_entries': manpower_entries,
+    })
+    
 
 
-# def update_program(request, program_id):
-#     program = get_object_or_404(Program, pk=program_id)
-
-#     if request.method == 'POST':
-#         kpi_names = request.POST.getlist('kpi_name[]')
-#         kpi_dates = request.POST.getlist('kpi_date[]')
-#         kpi_budgets = request.POST.getlist('kpi_budget[]')
-
-#         workshop_names = request.POST.getlist('workshop_name[]')
-#         workshop_dates = request.POST.getlist('workshop_date[]')
-#         workshop_budgets = request.POST.getlist('workshop_budget[]')
-
-#         meetings_names = request.POST.getlist('meeting_name[]')
-#         meetings_dates = request.POST.getlist('meeting_date[]')
-#         meetings_budgets = request.POST.getlist('meeting_budget[]')
-
-#         manpower_names = request.POST.getlist('manpower_details[]')
-#         manpower_dates = request.POST.getlist('manpower_date[]')
-#         manpower_budgets = request.POST.getlist('manpower_budget[]')
-
-#         for name, date, budget in zip(kpi_names, kpi_dates, kpi_budgets):
-#             Subentry.save()
-#         for name, date, budget in zip(workshop_names, workshop_dates, workshop_budgets):
-#             Subentry.save()
-#         for name, date, budget in zip(meetings_names, meetings_dates, meetings_budgets):
-#             Subentry.save()
-#         for name, date, budget in zip(manpower_names, manpower_dates, manpower_budgets):
-#             Subentry.save()
-
-#         # return redirect('program_detail', program_id=program.id)
-
-
-#     return render(request, 'user_dashboard/edit_program.html', {'program': program, 'update_mode':True})
+@csrf_exempt
+def delete_subentry(request, entry_id):
+    if request.method == "DELETE":
+        try:
+            Subentry.objects.get(id=entry_id).delete()
+            return JsonResponse({"success": True})
+        except Subentry.DoesNotExist:
+            return JsonResponse({"success": False, "error": "Not found"})
+    return JsonResponse({"success": False, "error": "Invalid request"})
